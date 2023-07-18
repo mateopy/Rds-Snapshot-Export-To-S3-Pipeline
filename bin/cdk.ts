ecfcb8cc024d4dc4351231b0456310849c82ff27
Mateo Núñez <mateo@web.com.py>
2023-07-18 19:01:18 -0400

    Test Project

--- bin/cdk.ts
+++ bin/cdk.ts
@@ -5,13 +5,20 @@ import { RdsSnapshotExportPipelineStack, RdsEventId, RdsSnapshotType } from '../
 
 const app = new cdk.App();
 new RdsSnapshotExportPipelineStack(app, 'RdsSnapshotExportToS3Pipeline', {
-  dbName: '<existing-rds-database-name>',
-  rdsEvents:
-    [
-      {
-        rdsEventId: RdsEventId.DB_AUTOMATED_SNAPSHOT_CREATED,
-        rdsSnapshotType: RdsSnapshotType.DB_AUTOMATED_SNAPSHOT
-      }
-    ],
-  s3BucketName: '<desired-s3-bucket-name>',
+  dbName: 'test-pg-database-1',
+  rdsEvents: [
+  {
+    rdsEventId: RdsEventId.DB_AUTOMATED_SNAPSHOT_CREATED,
+    rdsSnapshotType: RdsSnapshotType.DB_AUTOMATED_SNAPSHOT
+  },
+  {
+    rdsEventId: RdsEventId.DB_MANUAL_SNAPSHOT_CREATED,
+    rdsSnapshotType: RdsSnapshotType.DB_BACKUP_SNAPSHOT
+  },
+  {
+    rdsEventId: RdsEventId.DB_BACKUP_SNAPSHOT_FINISHED_COPY,
+    rdsSnapshotType: RdsSnapshotType.DB_BACKUP_SNAPSHOT
+  }
+],
+  s3BucketName: 'nxt-landingzone-saeast1-002-dev',
 });
